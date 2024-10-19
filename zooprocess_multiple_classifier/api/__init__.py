8eb86b4626a6df2dd8ededb5b737093d9710c513
Jean-Olivier Irisson <irisson@normalesup.org>
2024-10-19 08:59:56 +0000

    Next syntax of fields' description

--- zooprocess_multiple_classifier/api/__init__.py
+++ zooprocess_multiple_classifier/api/__init__.py
@@ -2,6 +2,9 @@ from webargs import fields
 
 schema = {
     "score": fields.Float(
-        required=True, description="in [0,1]: the probability for the image to be a multiple. A natural threshold to classify it as multiple is 0.5 but lowering this threshold can increase the recall of multiples, at the expense of precision."
+        required=True,
+        metadata={
+            'description': "in [0,1]: the probability for the image to be a multiple. A natural threshold to classify it as multiple is 0.5 but lowering this threshold can increase the recall of multiples, at the expense of precision."
+        }
     )
 }
