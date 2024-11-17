a5b732c0916f663885b4cc4c5f6962452c7f6dba
Jean-Olivier Irisson <irisson@normalesup.org>
2024-11-17 18:59:47 +0100

    More appropriate documentation of the output schema

--- zooprocess_multiple_classifier/api.py
+++ zooprocess_multiple_classifier/api.py
@@ -139,7 +139,12 @@ def predict(**kwargs):
 
 # Schema to validate the `predict()` output
 schema = {
-    "score": fields.Float()
+    "score": fields.Float(
+        required=True,
+        metadata={
+            'description': "in [0,1]: the probability for the image to be a multiple. A natural threshold to classify it as multiple is 0.5 but lowering this threshold can increase the recall of multiples, at the expense of precision."
+        }
+    )
 }
 
 # def get_train_args():
