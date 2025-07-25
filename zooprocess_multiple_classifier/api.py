3cc3085c1afc6ef87a586fba82a205af66177c65
Jean-Olivier Irisson <irisson@normalesup.org>
2025-07-25 22:47:25 +0000

    in_dir

--- zooprocess_multiple_classifier/api.py
+++ zooprocess_multiple_classifier/api.py
@@ -242,8 +242,8 @@ def train(**kwargs):
         Path to the model weights.
     """
     run_train(
-      in_dir=os.path.join(BASE_DIR, 'data'),
       out_dir=os.path.join(BASE_DIR, 'data'),
+      data_dir=os.path.join(BASE_DIR, 'data'),
       device=device,
       bottom_crop=kwargs['bottom_crop'],
       n_epochs=kwargs['n_epochs'],
